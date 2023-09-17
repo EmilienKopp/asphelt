fe2c8e4984889862312b054d9ce49c89ff830f31
EmilienKopp <emilien.kopp@gmail.com>
2023-09-17 14:37:30 +0900

    feat: storybook darkmode impl

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from "@storybook/svelte-vite";
+import addons from "@storybook/addons";
+
+// const channel = addons.useChannel({'DARK_MODE': isDark => document.querySelector('html')?.classList.toggle('dark', isDark)});
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
@@ -6,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    // 'storybook-dark-mode',
   ],
   framework: {
     name: "@storybook/svelte-vite",
